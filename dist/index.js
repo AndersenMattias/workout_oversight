de59cc898e75187eb815d0dcfc0fd50c5b066ca3
mattias.andersen@consid.se <mattias.andersen@consid.se>
2023-12-18 15:13:31 +0100

    feat: add connection to postgreSQL

--- dist/index.js
+++ dist/index.js
@@ -6,12 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const exercises_1 = require("./routes/exercises");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.get('/', (req, res) => {
-    res.send('Hello World From the Typescript Server!');
+app.use('/api', exercises_1.exerciseRouter);
+app.get('/', async (req, res) => {
+    //const exercises = await pool.query('SELECT * FROM exercises')
+    // res.json(exercises.rows)
+    res.json({ message: 'Hello from server side!' });
 });
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
